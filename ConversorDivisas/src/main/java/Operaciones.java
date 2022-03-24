ef1cbe61d443320d88dae8faad18f8e12d5cbe58
jorgerequeno <101310322+jorgerequeno@users.noreply.github.com>
2022-03-24 16:23:02 -0600

    Add files via upload

---
+++ ConversorDivisas/src/main/java/Operaciones.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author FRIDA KALO
+ */
+public class Operaciones {
+    private double valor, resultado;
+private int divisa, divisafinal;
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public int getDivisa() {
+        return divisa;
+    }
+
+    public void setDivisa(int divisa) {
+        this.divisa = divisa;
+    }
+
+    public int getDivisafinal() {
+        return divisafinal;
+    }
+
+    public void setDivisafinal(int divisafinal) {
+        this.divisafinal = divisafinal;
+    }
+
+    public double getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(double resultado) {
+        this.resultado = resultado;
+    }
+
+
+
+    public Operaciones() {
+    }
+
+
+
+public double convertir(){
+
+if (divisa ==0 && divisafinal == 1){
+resultado = valor * 0.041 ;
+
+}else if (divisa == 0 && divisafinal == 0){
+resultado = valor ;
+}
+
+return resultado;
+}
+}
